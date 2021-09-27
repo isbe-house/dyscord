ad6ac29121f4e38ff18fcce49f9843e26fd99c3e
John Murray <jmurrayufo@gmail.com>
2021-09-26 18:30:28 -0600

    Dev (#11)
    * chore: Add a bit more info to the README.md
    
    * fix: simple addition to test.

--- tests/objects/interactions/test_validation.py
+++ tests/objects/interactions/test_validation.py
@@ -8,3 +8,8 @@ def test_names():
     new_cmd.generate(name='Fail', description='Not valid', type=Command.COMMAND_TYPE.CHAT_INPUT)
     with pytest.raises(ValueError):
         new_cmd.validate()
+
+    new_cmd.generate(name='123123123412312312341231231234123', description='Not valid', type=Command.COMMAND_TYPE.CHAT_INPUT)
+    with pytest.raises(ValueError):
+        new_cmd.validate()
+
