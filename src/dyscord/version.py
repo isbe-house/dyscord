9c3e584820d6bae16da9b670a7893712e8301b9c
John Murray <jmurrayufo@gmail.com>
2021-10-18 12:00:41 -0600

    cicd: make regex a lot better for tagging.

--- src/dyscord/version.py
+++ src/dyscord/version.py
@@ -1,5 +1,6 @@
 '''Version of the software.'''
 
-__version__ = '0.2.0.dev5'
+__version__ = '0.2.0.dev6'
 
 # [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
+# Valid regex: ((\d+)(\.\d+(\.\d+)?)?)((a|b|rc)\d+)?(.post\d+)?(.dev\d+)?
