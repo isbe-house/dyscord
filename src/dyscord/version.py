fca9b6bbf5db60a2fa3bb73949bebb9fbb88f6ba
John Murray <jmurrayufo@gmail.com>
2021-10-18 12:57:59 -0600

    chore: roll version for first formal release.

--- src/dyscord/version.py
+++ src/dyscord/version.py
@@ -1,6 +1,6 @@
 '''Version of the software.'''
 
-__version__ = '0.2.0.dev7'
+__version__ = '0.2.0'
 
 # [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
 # Valid regex: ((\d+)(\.\d+(\.\d+)?)?)((a|b|rc)\d+)?(.post\d+)?(.dev\d+)?
