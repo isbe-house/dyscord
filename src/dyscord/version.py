2ddce6335e1c7d1cd5cfd7ba0acb6e6c1bace049
John Murray <jmurrayufo@gmail.com>
2021-10-22 16:18:55 -0600

    chore: sync version for release.

--- src/dyscord/version.py
+++ src/dyscord/version.py
@@ -1,6 +1,6 @@
 '''Version of the software.'''
 
-__version__ = '0.4.0.dev0'
+__version__ = '0.4.0'
 
 # [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
 # Valid regex: ((\d+)(\.\d+(\.\d+)?)?)((a|b|rc)\d+)?(.post\d+)?(.dev\d+)?
